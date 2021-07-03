7fe211ef1a393a3a6976eaa54b70ff193f413e02
wangfaxi <wangfaxi@grgbanking.com>
2021-07-03 17:21:00 +0800

    初始化项目工程

---
+++ cloudmall-coupon/src/test/java/com/fox/cloudmall/coupon/CloudmallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fox.cloudmall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudmallCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ cloudmall-product/src/test/java/com/fox/cloudmall/product/CloudmallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fox.cloudmall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudmallProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
